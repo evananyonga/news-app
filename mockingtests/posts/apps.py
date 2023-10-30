98e47919b23bfdf49d13014edec115ddc1cfa7df
evananyonga <evewish@gmail.com>
2023-10-30 16:17:54 +0300

    add posts application

--- mockingtests/posts/apps.py
+++ mockingtests/posts/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class HttptestConfig(AppConfig):
+class PostsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'httptest'
+    name = 'posts'
